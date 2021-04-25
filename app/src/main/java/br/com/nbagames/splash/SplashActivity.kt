4c79c38332577f3c48b1e31fb62bd41430f4243d
Bruno Mattoso Gonçalves <bruno.mattoso.goncalves@gmail.com>
2021-04-25 15:41:49 -0300

    Configure dependencies version.gradle file

--- app/src/main/java/br/com/nbagames/splash/SplashActivity.kt
+++ app/src/main/java/br/com/nbagames/splash/SplashActivity.kt
@@ -1,4 +1,4 @@
-package br.com.nbagames
+package br.com.nbagames.splash
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -10,7 +10,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import br.com.nbagames.ui.theme.NbaGamesTheme
 
-class MainActivity : ComponentActivity() {
+class SplashActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
