168b2ec723e50fd655cfc24b71d8e94f5ab0a485
Bruno Mattoso Gonçalves <bruno.mattoso.goncalves@gmail.com>
2021-05-31 20:26:15 -0300

    Add nba games logo to splash

--- app/src/main/java/br/com/nbagames/splash/Splash.kt
+++ app/src/main/java/br/com/nbagames/splash/Splash.kt
@@ -1,23 +1,23 @@
 package br.com.nbagames.splash
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import br.com.nbagames.R
 import br.com.nbagames.designsystem.components.RollingBall
-import br.com.nbagames.designsystem.components.TextField
 import br.com.nbagames.designsystem.theme.NbaGamesTheme
 import kotlinx.coroutines.delay
 
@@ -39,12 +39,12 @@ fun Splash(onSetupLoaded: () -> Unit) {
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        RollingBall()
-        TextField(
-            text = stringResource(id = R.string.app_name),
-            fontWeight = FontWeight.Bold,
-            color = Color.White
+        Image(
+            painter = painterResource(id = R.drawable.ic_nba_games_logo),
+            contentDescription = "NBA Games",
+            modifier = Modifier.size(200.dp)
         )
+        RollingBall()
     }
 }
 
