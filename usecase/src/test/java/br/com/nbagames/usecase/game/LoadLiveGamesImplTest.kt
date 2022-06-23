31c45d8a366b67365fb394d8cb30f5e056910f5b
bruno.mattoso <bruno.mattoso@thoughtworks.com>
2022-06-23 16:26:29 -0300

    Write test to LoadLiveGames UseCase

--- usecase/src/test/java/br/com/nbagames/usecase/game/LoadLiveGamesImplTest.kt
+++ usecase/src/test/java/br/com/nbagames/usecase/game/LoadLiveGamesImplTest.kt
@@ -1,9 +1,11 @@
 package br.com.nbagames.usecase.game
 
+import br.com.nbagames.repository.game.GameRepository
 import br.com.nbagames.usecase.game.fake.FakeGameRepository
+import com.google.common.truth.Truth.assertThat
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.test.runBlockingTest
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
 
@@ -12,17 +14,45 @@ internal class LoadLiveGamesImplTest {
 
     private lateinit var loadLiveGames: LoadLiveGames
 
-    @BeforeEach
-    fun setup() {
-        loadLiveGames = LoadLiveGamesImpl(
-            gameRepository = FakeGameRepository()
-        )
-    }
-
     @Test
     fun `Run without throwable expected`() {
+        instantiateLoadLiveGamesUseCase(FakeGameRepository())
+
         assertDoesNotThrow {
             runBlockingTest { loadLiveGames() }
         }
+    }
+
+    @Test
+    fun `Receive game list into flow`() {
+        val fakeGameRepository = FakeGameRepository()
+        val expectedGameList = fakeGameRepository.populateGameList()
+        instantiateLoadLiveGamesUseCase(fakeGameRepository)
+
+        runBlockingTest {
+            val gameListFlow = loadLiveGames()
+            gameListFlow.collect { gameList ->
+                assertThat(gameList).isEqualTo(expectedGameList)
+            }
+        }
+    }
+
+    @Test
+    fun `Receive empty game list into flow`() {
+        val fakeGameRepository = FakeGameRepository().apply { clearGameList() }
+        instantiateLoadLiveGamesUseCase(fakeGameRepository)
+
+        runBlockingTest {
+            val gameListFlow = loadLiveGames()
+            gameListFlow.collect { gameList ->
+                assertThat(gameList).isEmpty()
+            }
+        }
+    }
+
+    private fun instantiateLoadLiveGamesUseCase(gameRepository: GameRepository) {
+        loadLiveGames = LoadLiveGamesImpl(
+            gameRepository = gameRepository
+        )
     }
 }
--- usecase/src/test/java/br/com/nbagames/usecase/game/fake/FakeGameRepository.kt
+++ usecase/src/test/java/br/com/nbagames/usecase/game/fake/FakeGameRepository.kt
@@ -1,15 +1,50 @@
 package br.com.nbagames.usecase.game.fake
 
 import br.com.nbagames.model.Game
+import br.com.nbagames.model.Quarter
+import br.com.nbagames.model.Team
 import br.com.nbagames.repository.game.GameRepository
 
 class FakeGameRepository : GameRepository {
 
-    override suspend fun getLiveGamesFromRemote(): List<Game> {
-        return emptyList()
+    private val gameList: MutableList<Game> = mutableListOf()
+
+    override suspend fun getLiveGamesFromRemote(): List<Game> = gameList
+
+    override suspend fun getLiveGamesFromCache(): List<Game> = gameList
+
+    fun clearGameList() {
+        gameList.clear()
     }
 
-    override suspend fun getLiveGamesFromCache(): List<Game> {
-        return emptyList()
+    fun populateGameList(): List<Game> {
+        val homeTeam = Team(
+            id = 100,
+            name = "Brooklyn Nets",
+            nickname = "BKN",
+            logo = ""
+        )
+        val visitorTeam = Team(
+            id = 200,
+            name = "Miami Heat",
+            nickname = "MIA",
+            logo = ""
+        )
+
+        gameList.addAll(
+            listOf(
+                Game(
+                    id = 123,
+                    homeTeam = homeTeam,
+                    visitorTeam = visitorTeam,
+                    homePoints = 130,
+                    visitorPoints = 127,
+                    currentClock = "00:23",
+                    quarter = Quarter.Fourth
+                )
+            )
+        )
+
+        return gameList
     }
 }
