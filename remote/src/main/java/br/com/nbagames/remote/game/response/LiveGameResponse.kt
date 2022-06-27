4f6cbbc3074bd4042f0ff82afaf3a985bc7fac7d
bruno.mattoso <bruno.mattoso@thoughtworks.com>
2022-06-27 19:34:20 -0300

    Remove unused files

--- remote/src/main/java/br/com/nbagames/remote/game/response/LiveGameResponse.kt
+++
@@ -1,9 +0,0 @@
-package br.com.nbagames.remote.game.response
-
-import kotlinx.serialization.SerialName
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class LiveGameResponse(
-    @SerialName("response") val liveGameApiResponse: GameListResponse
-)
