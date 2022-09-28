8b239ad4279608858aea170a36c30c751c457a2a
Bruno Mattoso <bruno.mattoso@thoughtworks.com>
2022-09-28 15:08:43 -0300

    Add mockk dependency to fix DI unit tests

--- app/src/test/java/br/com/nbagames/init/KoinInitializerTest.kt
+++ app/src/test/java/br/com/nbagames/init/KoinInitializerTest.kt
@@ -2,6 +2,11 @@ package br.com.nbagames.init
 
 import android.app.Application
 import com.google.common.truth.Truth.assertThat
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.mockkStatic
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.koin.test.check.checkModules
@@ -9,9 +14,15 @@ import org.koin.test.check.checkModules
 internal class KoinInitializerTest {
 
     private lateinit var koinInitializer: KoinInitializer
+    private val firebaseDatabase: FirebaseDatabase = mockk(relaxed = true)
+    private val databaseReference: DatabaseReference = mockk(relaxed = true)
 
     @BeforeEach
     fun setup() {
+        mockkStatic(FirebaseDatabase::class)
+        every { FirebaseDatabase.getInstance() } answers { firebaseDatabase }
+        every { firebaseDatabase.reference } answers { databaseReference }
+
         koinInitializer = KoinInitializer()
     }
 
--- remote/src/test/java/br/com/nbagames/remote/di/NetworkModuleTest.kt
+++ remote/src/test/java/br/com/nbagames/remote/di/NetworkModuleTest.kt
@@ -1,5 +1,10 @@
 package br.com.nbagames.remote.di
 
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.mockkStatic
 import kotlinx.serialization.ExperimentalSerializationApi
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -10,12 +15,20 @@ import org.koin.test.check.checkModules
 class NetworkModuleTest {
 
     private lateinit var koinApplication: KoinApplication
+    private val firebaseDatabase: FirebaseDatabase = mockk(relaxed = true)
+    private val databaseReference: DatabaseReference = mockk(relaxed = true)
 
     @ExperimentalSerializationApi
     @BeforeEach
     fun setup() {
+        mockkStatic(FirebaseDatabase::class)
+        every { FirebaseDatabase.getInstance() } answers { firebaseDatabase }
+        every { firebaseDatabase.reference } answers { databaseReference }
+
         koinApplication = startKoin {
-            modules(NetworkModule.getNetworkModule("https://api-nba-v1.p.rapidapi.com/", "", ""))
+            modules(
+                NetworkModule.getNetworkModule("https://api-nba-v1.p.rapidapi.com/", "", "")
+            )
         }
     }
 
